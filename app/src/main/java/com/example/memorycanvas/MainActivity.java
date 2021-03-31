7e034e856b70668cfcf1cff3a897c36acf7504ec
appplepi3 <karpoff.parkour@yandex.ru>
2021-03-31 15:49:09 +0800

    Game Momorine, Android App, API>26

---
+++ app/src/main/java/com/example/memorycanvas/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.memorycanvas;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    TilesView view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        view = findViewById(R.id.view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_game:
+                view.newGame();
+                break;
+            case R.id.exit:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/memorycanvas/TilesView.java
@@ -0,0 +1,198 @@
+package com.example.memorycanvas;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.os.AsyncTask;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.ColorRes;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Card {
+    Paint p = new Paint();
+    private float width;
+    private float height;
+    private boolean visible = true;
+    public int getColor() {
+        return this.color;
+    }
+    public Card(int color) {
+        this.color = color;
+    }
+    public void flWidth(float width) {
+        this.width = width;
+    }
+    public void flHeight(float height) {
+
+        this.height = height;
+    }
+    public boolean visible() {
+        return visible;
+    }
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+    public void fl_x(float x) {
+        this.x = x;
+    }
+    public void fl_y(float y) {
+        this.y = y;
+    }
+    int color, backColor = R.color.backcard;
+    boolean isOpen = false;
+    float x, y;
+    @SuppressLint("ResourceAsColor")
+    public void draw(Canvas c) {
+        if (isOpen) {
+            p.setColor(color);
+        } else p.setColor(backColor);
+        c.drawRect(x,y, x+width, y+height, p);
+    }
+    public boolean flip (float touch_x, float touch_y) {
+        if (touch_x >= x && touch_x <= x + width && touch_y >= y && touch_y <= y + height) {
+            isOpen = ! isOpen;
+            return true;
+        } else return false;
+    }
+}
+
+public class TilesView extends View {
+    final int PAUSE_LENGTH = 2;
+    boolean isOnPauseNow = false;
+    int[] tiles = new int[]{Color.CYAN, Color.RED, Color.BLACK, Color.MAGENTA, Color.BLUE, Color.GREEN, Color.YELLOW, Color.TRANSPARENT, Color.DKGRAY, Color.WHITE, Color.LTGRAY, Color.GRAY};
+    int openedCard = 0;
+    ArrayList<Card> cards;
+    int width, height;
+    public TilesView(Context context) {
+        super(context);
+    }
+
+    public TilesView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        newGame();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        width = canvas.getWidth();
+        height = canvas.getHeight();
+
+        float cardWidth = (width * 0.87f) / 4;
+        float cardHeight = (height * 0.88f) / 6;
+        float indentWidth = width * 0.1f / 4;
+        float indentHeight = height * 0.1f / 6;
+        int counter = 0;
+        for (int i = 0; i < 4; i++){
+            for (int j = 0; j < 6; j++) {
+                Card CdHold = cards.get(counter);
+                if (!CdHold.visible()){
+                    CdHold.flWidth(0);
+                    CdHold.flHeight(0);
+                    counter++;
+                    continue;
+                }
+                CdHold.flHeight(cardHeight);
+                CdHold.flWidth(cardWidth);
+                CdHold.fl_x(cardWidth * i + indentWidth * (i + 1));
+                CdHold.fl_y(cardHeight * j + indentHeight * (j + 1));
+                counter++;
+            }
+        }
+        for (Card c: cards) {
+            c.draw(canvas);
+        }
+    }
+
+    private boolean Winner(){
+        for (Card c : cards){
+            if (c.visible())
+                return true;
+        }
+        return false;
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        if (event.getAction() == MotionEvent.ACTION_DOWN && !isOnPauseNow)
+        {
+            for (Card c: cards) {
+
+                if (openedCard == 0) {
+                    if (c.flip(x, y)) {
+                        openedCard ++;
+                        invalidate();
+                        return true;
+                    }
+                }
+
+                if (openedCard == 1) {
+                    if (c.flip(x, y)) {
+                        openedCard ++;
+                        invalidate();
+                        PauseTask task = new PauseTask();
+                        task.execute(PAUSE_LENGTH);
+                        isOnPauseNow = true;
+                        return true;
+                    }
+                }
+
+            }
+        }
+        return true;
+    }
+
+    public void newGame() {
+        cards = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            cards.add(new Card(tiles[i]));
+            cards.add(new Card(tiles[i]));
+        }
+        Collections.shuffle(cards);
+        invalidate();
+    }
+
+    class PauseTask extends AsyncTask<Integer, Void, Void> {
+        @Override
+        protected Void doInBackground(Integer... integers) {
+            try {
+                Thread.sleep(integers[0] * 1000);
+            } catch (InterruptedException e) {}
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            ArrayList<Card> CdHold = new ArrayList<>();
+            for (Card c: cards) {
+                if (c.isOpen) {
+                    CdHold.add(c);
+                    c.isOpen = false;
+                }
+            }
+            if (CdHold.get(0).getColor() == CdHold.get(1).getColor()){
+                CdHold.get(0).setVisible(false);
+                CdHold.get(1).setVisible(false);
+            }
+            if (!Winner()){
+                Toast.makeText(getContext(), "Вы выиграли!!!", Toast.LENGTH_LONG).show();
+            }
+            openedCard = 0;
+            isOnPauseNow = false;
+            invalidate();
+        }
+    }
+}
